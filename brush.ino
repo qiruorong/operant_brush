7fbf91c5c16e0ae4080ba401d8308ceabd134350
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-17 15:08:39 -0400

    Update brush.ino

--- brush.ino
+++ brush.ino
@@ -1,19 +1,19 @@
 #include <AFMotor.h>
-
-// define enA = 9
-// define in1 = 6
-// define in2 = 7
+AF_DCMotor motor(1);
+int enA = 9;
+int in1 = 6;
+int in2 = 7;
 
 void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(button, INPUT);
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
 
   Serial.begin(9600);
 }
-
-AF_DCMotor motor(1); // channel 1
 
 void setup() {
   motor.setSpeed(100); // 0-255
