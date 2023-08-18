c520542ba711473e8e9158ac430da456881df1c4
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-18 11:12:23 -0400

    Update brush.ino

--- brush.ino
+++ brush.ino
@@ -1,24 +1,21 @@
 #include <AFMotor.h>
 AF_DCMotor motor(1);
 int enA = 9;
-int in1 = 6;
+int in1 = 8;
 int in2 = 7;
 
 void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  pinMode(button, INPUT);
 	digitalWrite(in1, LOW);
 	digitalWrite(in2, LOW);
+  motor.setSpeed(100); // 0-255
+  randomSeed(analogRead(0)); 
 
   Serial.begin(9600);
 }
 
-void setup() {
-  motor.setSpeed(100); // 0-255
-  randomSeed(analogRead(0)); 
-}
 
 void loop() {
   int randomNumber = random(2); 
