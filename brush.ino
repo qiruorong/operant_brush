9358a2719fa44de549b136ca54e41654124cbc6c
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-21 11:56:51 -0400

    Update brush.ino

--- brush.ino
+++ brush.ino
@@ -1,29 +1,34 @@
-const int motorPin1 = 9;  // Motor control pin 1
-const int motorPin2 = 10; // Motor control pin 2
+int enA = 9;
+int in1 = 5;
+int in2 = 4;
 
 void setup() {
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  randomSeed(analogRead(0));  // Seed the random number generator
-  
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  randomSeed(analogRead(0));
+
   Serial.begin(9600); // Initialize serial communication
 }
 
 void loop() {
-  int randomNum = random(2); // Generate a random number: 0 or 1
-  
-  // Send the random number to the computer via serial communication
+  int randomNum = random(2);
   Serial.println(randomNum);
 
-  if (randomNum == 0) {
-    // Turn the motor in one direction
-    digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
-  } else {
-    // Turn the motor in the other direction
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, HIGH);
-  }
+  analogWrite(enA, 255);
 
-  delay(2000); // Wait for 2 seconds before changing direction
+  if (randomNum == 0) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    delay(1000);
+  } else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    delay(1000);
+  }
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  delay(1000);
 }
