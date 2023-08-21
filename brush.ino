a4cfb5f9a8e85edab3eed4ad8d5a739da87181ad
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-21 10:49:42 -0400

    Update brush.ino

--- brush.ino
+++ brush.ino
@@ -1,68 +1,29 @@
-#include <AFMotor.h>
-int enA = 9;
-int in1 = 4;
-int in2 = 5;
-
+const int motorPin1 = 9;  // Motor control pin 1
+const int motorPin2 = 10; // Motor control pin 2
 
 void setup() {
-	// Set all the motor control pins to outputs
-	pinMode(enA, OUTPUT);
-	pinMode(in1, OUTPUT);
-	pinMode(in2, OUTPUT);
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  randomSeed(analogRead(0));  // Seed the random number generator
+  
+  Serial.begin(9600); // Initialize serial communication
 }
 
 void loop() {
-	directionControl();
-	delay(1000);
-	speedControl();
-	delay(1000);
-}
+  int randomNum = random(2); // Generate a random number: 0 or 1
+  
+  // Send the random number to the computer via serial communication
+  Serial.println(randomNum);
 
-// This function lets you control spinning direction of motors
-void directionControl() {
-	// Set motors to maximum speed
-	// For PWM maximum possible values are 0 to 255
-	analogWrite(enA, 255);
+  if (randomNum == 0) {
+    // Turn the motor in one direction
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+  } else {
+    // Turn the motor in the other direction
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+  }
 
-
-	// Turn on motor A & B
-	digitalWrite(in1, HIGH);
-	digitalWrite(in2, LOW);
-
-	delay(2000);
-	
-	// Now change motor directions
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-
-	delay(2000);
-	
-	// Turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-}
-
-// This function lets you control speed of the motors
-void speedControl() {
-	// Turn on motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	
-	// Accelerate from zero to maximum speed
-	for (int i = 0; i < 256; i++) {
-		analogWrite(enA, i);
-		delay(20);
-	}
-	
-	// Decelerate from maximum speed to zero
-	for (int i = 255; i >= 0; --i) {
-		analogWrite(enA, i);
-		delay(20);
-	}
-	
-	// Now turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
+  delay(2000); // Wait for 2 seconds before changing direction
 }
