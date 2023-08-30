1ca10efe870de99c6492d741a6f9df077c94bac4
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-30 15:24:12 -0400

    Update serialAccess.py

--- serialAccess.py
+++ serialAccess.py
@@ -1,15 +1,26 @@
 import serial
 
 arduino_port = 'COM11'
-
 ser = serial.Serial(arduino_port, 9600)
+file_path = "C:/Users/rqi9/Desktop/trial_ids(m).txt"
 
-with open('data.txt', 'w') as file:
+with open(file_path, "a") as file:
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode().strip()
-            print(line)
-            file.write(line + '\n')
+            data = line.split(",") 
+
+            if len(data) == 2:  
+                randomNum = data[0]
+                stimTimestamp = data[1]
+            print(randomNum)
+            print(stimTimestamp)
+            
+            # Write them as separate lines in the file
+            file.write("Random Number: " + randomNum + '\n')
+            file.write("Stimulus Timestamp: " + stimTimestamp + '\n')
+            file.flush() 
 
 ser.close()
+
 
