8ea59d9a27505868f7202a6ad270f9e38036b9e2
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-10-06 01:19:39 -0400

    Create GUI*.py

---
+++ GUI*.py
@@ -0,0 +1,4 @@
+# in stage 1 of this experiment, mice are trained to lick
+# in stage 2 of this experiment, mice are trained to lick when stimulus on paw
+# in stage 3 of this experiment, mice are trained to lick associate with one stimulus when only that stimulus is present
+# in stage 4 of this experiment, mice are trained to lick associate with one stimulus when the other direction is present
